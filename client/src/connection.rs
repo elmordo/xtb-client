a295460b392315bf981b7e9ce2fcf23c14771f4e
Petr Jindra <petr@gbol.cz>
2024-03-03 21:30:44 +0100

    Refactor connection.rs and add tests
    Refactored the connection.rs file including changing RwLock to Mutex, and adding new functions, error types, and structs. Amplified functionality of ResponsePromise with new implementation and state management. Moreover, added comprehensive tests for ResponsePromises and TagMaker functionalities. The required dependencies for the tests were also added to the Cargo.toml file.

--- client/src/connection.rs
+++ client/src/connection.rs
@@ -1,19 +1,21 @@
 use std::future::Future;
-use std::pin::Pin;
+use std::ops::Deref;
+use std::pin::{Pin, pin};
 use std::sync::Arc;
-use std::task::{Context, Poll};
+use std::task::{Context, Poll, Waker};
+
 use async_trait::async_trait;
-use futures_util::stream::{SplitSink, SplitStream};
 use futures_util::{SinkExt, StreamExt};
+use futures_util::stream::{SplitSink, SplitStream};
 use serde_json::Value;
 use thiserror::Error;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
-use tokio::sync::RwLock;
-use tokio_tungstenite::{connect_async, connect_async_with_config, MaybeTlsStream, WebSocketStream};
+use tokio::sync::Mutex;
+use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
 use tokio_tungstenite::tungstenite::Message;
-use tokio_tungstenite::tungstenite::protocol::WebSocketConfig;
 use url::Url;
+
 use crate::api::{ErrorResponse, Request, Response};
 
 #[async_trait]
@@ -31,7 +33,7 @@ pub enum XtbConnectionError {
     #[error("Cannot serialize command payload")]
     SerializationError(serde_json::Error),
     #[error("Cannot send request to the XTB server.")]
-    CannotSendRequest(tokio_tungstenite::tungstenite::Error)
+    CannotSendRequest(tokio_tungstenite::tungstenite::Error),
 }
 
 
@@ -88,33 +90,85 @@ impl XtbConnection for BasicXtbConnection {
 }
 
 
-type PromisedResponseData = Option<Result<Response, ErrorResponse>>;
-
-
+/// Internal state shared between the ResponsePromise and BasicXtbConnection instance.
+/// This state is used to deliver response to the consumer.
 #[derive(Debug)]
-pub struct PromisedResponse {
-    response: Arc<RwLock<PromisedResponseData>>,
+struct ResponsePromiseState {
+    /// The response.
+    ///
+    /// * `None` - the response is not ready yet.
+    /// * `Some(response)` - the response is ready to be delivered.
+    response: Option<Result<Response, ErrorResponse>>,
+    /// If the `ResponsePromise` was palled, the `Waker` is stored here.
+    /// When response is set and the waker is set, the waker is called.
+    waker: Option<Waker>,
 }
 
 
-impl PromisedResponse {
-    pub fn new() -> (Self, Arc<RwLock<PromisedResponseData>>) {
-        let data = Arc::new(RwLock::new(PromisedResponseData::default()));
-        (Self { response: data.clone() }, data)
+impl ResponsePromiseState {
+    /// Create new instance waiting for a response
+    pub fn new() -> Self {
+        Self { response: None, waker: None }
+    }
+
+    /// Set response. If a waker is set in the state, it is notified.
+    pub fn set_response(&mut self, response: Result<Response, ErrorResponse>) {
+        self.response = Some(response);
+        if let Some(waker) = self.waker.take() {
+            waker.wake();
+        }
     }
 }
 
 
-impl Future for PromisedResponse {
+#[derive(Debug)]
+pub struct ResponsePromise {
+    state: Arc<Mutex<ResponsePromiseState>>,
+}
+
+
+/// Represent promise of a response delivery in a future.
+///
+/// Implements the `Future` trait and when the future is awaited, it is resolved by response
+/// returned from a server. The response is type of `Result<Response, ErrorResponse>`.
+impl ResponsePromise {
+    /// Create new instance and return tuple:
+    ///
+    /// 1. instance of `Self`
+    /// 2. thread safe `ResponsePromiseState` for response delivery.
+    pub fn new() -> (Self, Arc<Mutex<ResponsePromiseState>>) {
+        let state = ResponsePromiseState::new();
+        let wrapped_state = Arc::new(Mutex::new(state));
+        (Self { state: wrapped_state.clone() }, wrapped_state)
+    }
+}
+
+
+impl Future for ResponsePromise {
     type Output = Result<Response, ErrorResponse>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        // Try to get the lock
+        if let Poll::Ready(mut guard) = pin!(self.state.lock()).poll(cx) {
+            // If response is set, return it as `Poll::Ready`
+            if let Some(response) = guard.response.take() {
+                return Poll::Ready(response);
+            }
+            // If response is not ready yet, register the waker.
+            guard.waker = Some(cx.waker().clone());
+        }
+        // Wait until response is ready
         Poll::Pending
     }
 }
 
 
 /// Helper struct generating message tags.
+///
+/// It generates unique tags with prefix "message_" followed by incremented positive integer number.
+/// The increment step is 1 and the first number is 1.
+///
+/// Example of series is: "message_1", "message_2", "message_3", ...
 #[derive(Default, Debug)]
 struct TagMaker(u64);
 
@@ -123,5 +177,63 @@ impl TagMaker {
     fn next(&mut self) -> String {
         self.0 += 1;
         format!("message_{}", self.0.to_string())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    mod response_promise {
+        use std::sync::Arc;
+        use std::time::Duration;
+
+        use rstest::*;
+        use serde_json::to_value;
+        use tokio::spawn;
+        use tokio::sync::Mutex;
+        use tokio::time::sleep;
+
+        use crate::api::Response;
+        use crate::connection::ResponsePromiseState;
+        use crate::ResponsePromise;
+
+        #[rstest]
+        #[case(0)]
+        #[case(1)]
+        #[case(50)]
+        #[case(100)]
+        #[timeout(Duration::from_millis(500))]
+        #[tokio::test]
+        async fn deliver_data(#[case] delay_ms: u64) {
+            let (instance, target) = ResponsePromise::new();
+            spawn(write_data(target, delay_ms));
+            let result = instance.await;
+        }
+
+        async fn write_data(target: Arc<Mutex<ResponsePromiseState>>, delay: u64) {
+            if delay > 0 {
+                sleep(Duration::from_millis(delay)).await;
+            }
+            let mut lock = target.lock().await;
+            let mut response = Response::default();
+            response.return_data = Some(to_value(42).unwrap());
+            lock.set_response(Ok(response));
+        }
+    }
+
+    mod tag_maker {
+        use crate::connection::TagMaker;
+
+        #[test]
+        fn make_series() {
+            let mut maker = TagMaker::default();
+
+            let tag = maker.next();
+            assert_eq!(tag, "message_1");
+            let tag = maker.next();
+            assert_eq!(tag, "message_2");
+            let tag = maker.next();
+            assert_eq!(tag, "message_3");
+        }
     }
 }
