0d4c3647b51009ef957ad1610e9985edb3aee61a
Petr Jindra <petr@gbol.cz>
2024-03-05 14:45:17 +0100

    Refactor message processing in connection.rs
    This update refactors the message processing in connection.rs making it more robust. It includes new error handling for conditions like receiving invalid data or errors when reading a response, but also improves existing error handling with more specific error messages. Invalid or untagged responses are now properly handled and logged. The logic for processing received messages and validating responses is separated into distinct functions for increased clarity.

--- client/src/connection.rs
+++ client/src/connection.rs
@@ -7,14 +7,14 @@ use std::task::{Context, Poll, Waker};
 use async_trait::async_trait;
 use futures_util::{SinkExt, StreamExt};
 use futures_util::stream::{SplitSink, SplitStream};
-use serde_json::{from_str, from_value, Value};
+use serde_json::{from_str, from_value, Map, Value};
 use thiserror::Error;
 use tokio::net::TcpStream;
 use tokio::spawn;
 use tokio::sync::Mutex;
 use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
 use tokio_tungstenite::tungstenite::Message;
-use tracing::error;
+use tracing::{error, warn};
 use url::Url;
 
 use crate::api::{ErrorResponse, Request, Response, XtbErrorCodeError};
@@ -37,6 +37,10 @@ pub enum XtbConnectionError {
     DeserializationError(serde_json::Error),
     #[error("Cannot send request to the XTB server.")]
     CannotSendRequest(tokio_tungstenite::tungstenite::Error),
+    #[error("Error when reading response from the XTB server.")]
+    ReceiveError(tokio_tungstenite::tungstenite::Error),
+    #[error("Expected to receive text data but something wrong was received.")]
+    ReceivedInvalidData(tokio_tungstenite::tungstenite::Error),
     #[error("The operation failed and server return error response")]
     OperationFailed(ErrorResponse),
     #[error("A response format is malformed: {0}")]
@@ -87,13 +91,29 @@ impl BasicXtbConnection {
         let lookup = self.promise_state_by_tag.clone();
         spawn(async move {
             while let Some(message) = stream.next().await {
-                let (response, tag) = match process_message(message) {
-                    Some((r, t)) => (r, t),
-                    None => continue,
+                let response = match process_message(message) {
+                    Ok(response) => Ok(response),
+                    Err(XtbConnectionError::OperationFailed(err_response)) => Err(err_response),
+                    Err(err) => {
+                        error!("Cannot process response: {:?}", err);
+                        continue
+                    },
+                };
+                let maybe_tag = match response.as_ref() {
+                    Ok(resp) => resp.custom_tag.as_ref(),
+                    Err(resp) => resp.custom_tag.as_ref(),
                 };
 
-                if let Some(state) = lookup.lock().await.remove(&tag) {
-                    state.lock().await.set_response(response);
+                let tag = match maybe_tag {
+                    Some(t) => t,
+                    _ => {
+                        warn!("Response has no tag and cannot be routed: {:?}", response);
+                        continue;
+                    }
+                };
+
+                if let Some(state) = lookup.lock().await.remove(tag) {
+                    state.lock().await.set_response(response.map_err(|err| XtbConnectionError::OperationFailed(err)));
                 }
             }
         });
@@ -101,23 +121,22 @@ impl BasicXtbConnection {
 }
 
 
-fn process_message(message: Result<Message, tokio_tungstenite::tungstenite::Error>) -> Option<(Result<Response, XtbConnectionError>, String)> {
+fn process_message(message: Result<Message, tokio_tungstenite::tungstenite::Error>) -> Result<Response, XtbConnectionError> {
     let message = match message {
         Ok(msg) => msg,
         Err(err) => {
             error!("Error when receiving message: {:?}", err);
-            return None;
+            return Err(XtbConnectionError::ReceiveError(err));
         }
     };
 
-    let text_content = message.to_text().ok()?;
-    let v: Value = from_str(text_content).ok()?;
-    let tag = v.as_object()?.get("customTag")?.as_str()?.to_owned();
-    Some((process_value(v), tag))
+    let text_content = message.to_text().map_err(|err| XtbConnectionError::ReceivedInvalidData(err))?;
+    let value = from_str(text_content).map_err(|err| XtbConnectionError::DeserializationError(err))?;
+    process_message_value(value)
 }
 
 
-fn process_value(value: Value) -> Result<Response, XtbConnectionError> {
+fn process_message_value(value: Value) -> Result<Response, XtbConnectionError> {
     let status = value
         .as_object().ok_or_else(|| XtbConnectionError::MalformedResponse("Value is not object".to_string()))?
         .get("status").ok_or_else(|| XtbConnectionError::MalformedResponse("The response 'status' field is missing".to_owned()))?
@@ -242,6 +261,74 @@ impl TagMaker {
 
 #[cfg(test)]
 mod tests {
+    mod process_message_and_process_message_value {
+        use rstest::rstest;
+        use serde_json::{from_str, Value};
+        use tokio_tungstenite::tungstenite::Message;
+
+        use crate::api::XtbErrorCode;
+        use crate::connection::process_message;
+        use crate::XtbConnectionError;
+
+        #[rstest]
+        #[case(r#"{"returnData": {"field": 12}, "status": true, "customTag": "myTag"}"#, "myTag", true)]
+        #[case(r#"{"status": true, "customTag": "myTag"}"#, "myTag", false)]
+        fn process_valid_response(#[case] payload: &str, #[case] expected_tag: &str, #[case] has_data: bool) {
+            let msg = Message::text(payload);
+            let response = process_message(Ok(msg)).unwrap();
+            let tag = response.custom_tag.clone().unwrap();
+            let expected_data = from_str::<Value>(payload).unwrap().as_object().unwrap().get("returnData").map(|v| v.to_owned());
+
+            if has_data {
+                assert!(response.return_data.is_some());
+            } else {
+                assert!(response.return_data.is_none());
+            }
+
+            assert!(response.status);
+            assert_eq!(&tag, expected_tag);
+            assert_eq!(response.return_data, expected_data);
+        }
+
+        #[rstest]
+        #[case(r#"{"status": false, "errorCode": "BE001", "errorDescr": "BE001", "customTag": "myTag"}"#, "myTag", XtbErrorCode::BE001)]
+        fn process_valid_error_response(#[case] payload: &str, #[case] expected_tag: &str, #[case] expected_error: XtbErrorCode) {
+            let msg = Message::text(payload);
+            let err = process_message(Ok(msg)).unwrap_err();
+
+            let error_response = match err {
+                XtbConnectionError::OperationFailed(e) => e,
+                _ => panic!("Invalid error")
+            };
+
+            let tag = error_response.custom_tag.unwrap();
+            assert_eq!(&tag, expected_tag);
+            assert_eq!(error_response.error_code, expected_error);
+            assert!(error_response.error_descr.unwrap().len() > 0);
+        }
+
+        #[test]
+        fn process_invalid_message_not_json() {
+            let msg = Message::text(r#"{foo bar}"#);
+            let err = process_message(Ok(msg)).unwrap_err();
+            match err {
+                XtbConnectionError::DeserializationError(_) => (),
+                _ => panic!("Expected XtbConnectionError::DeserializationError")
+            };
+        }
+
+        #[rstest]
+        #[case(r#"{"errorCode": "BE001", "errorDescr": "BE001", "customTag": "myTag"}"#)]
+        fn process_invalid_message_malformed_response(#[case] payload: &str) {
+            let msg = Message::text(payload);
+            let err = process_message(Ok(msg)).unwrap_err();
+            match err {
+                XtbConnectionError::MalformedResponse(_) => (),
+                _ => panic!("Expected XtbConnectionError::MalformedResponse, but {:?}", err)
+            };
+        }
+    }
+
     mod response_promise {
         use std::sync::Arc;
         use std::time::Duration;
