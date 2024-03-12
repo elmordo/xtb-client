3e3be40d67fb471c08ebb830027f69c7043734a4
Petr Jindra <petr@gbol.cz>
2024-03-12 09:54:27 +0100

    Refactor `XtbStreamConnection` and implement message building methods
    The `XtbStreamConnection` interface is refactored by adjusting argument types for `subscribe` and `unsubscribe` methods and implementing additional methods for building and sending messages. Argument value must now be `Value::Object` and can be optional. A helper function `prepare_arguments` is added to validate and prepare arguments for the message. Also, an error type `InvalidArgumentsType` is included to handle inappropriate argument types.

--- client/src/stream_connection.rs
+++ client/src/stream_connection.rs
@@ -1,7 +1,7 @@
 use async_trait::async_trait;
 use futures_util::stream::SplitSink;
-use futures_util::StreamExt;
-use serde_json::Value;
+use futures_util::{SinkExt, StreamExt};
+use serde_json::{Map, Value};
 use thiserror::Error;
 use tokio::sync::broadcast::{channel, Sender};
 use tokio_tungstenite::connect_async;
@@ -9,20 +9,20 @@ use tokio_tungstenite::tungstenite::Message;
 use url::Url;
 
 use crate::listener::Stream;
-use crate::message_processing::ProcessedMessage;
+use crate::message_processing::{ProcessedMessage};
 
 /// Common interface for stream command api of the XTB.
 #[async_trait]
 pub trait XtbStreamConnection {
     /// Subscribe for data stream from the XTB server
     ///
-    /// The `arguments` must be `Value::Null` or `Value::Object`. Any other variants causes an error
-    async fn subscribe(&mut self, command: &str, arguments: Value) -> Result<(), XtbStreamConnectionError>;
+    /// The `arguments` must be `Value::Object`. Any other variants causes an error
+    async fn subscribe(&mut self, command: &str, arguments: Option<Value>) -> Result<(), XtbStreamConnectionError>;
 
     /// Unsubscribe from data stream from the XTB server
     ///
-    /// The `arguments` must be `Value::Null` or `Value::Object`. Any other variants causes an error
-    async fn unsubscribe(&mut self, command: &str, arguments: Value) -> Result<(), XtbStreamConnectionError>;
+    /// The `arguments` must be `Value::Object`. Any other variants causes an error
+    async fn unsubscribe(&mut self, command: &str, arguments: Option<Value>) -> Result<(), XtbStreamConnectionError>;
 
     /// Create message stream builder
     async fn make_message_stream(&mut self, filter: StreamFilter) -> MessageStream;
@@ -30,8 +30,10 @@ pub trait XtbStreamConnection {
 
 
 pub struct BasicXtbStreamConnection {
+    /// Stream session id used to identify for the stream server
     stream_session_id: String,
     sender: Sender<ProcessedMessage>,
+    /// Sink used for sending messages to the XTB server
     sink: SplitSink<Stream, Message>,
 }
 
@@ -48,17 +50,44 @@ impl BasicXtbStreamConnection {
             sink
         })
     }
+
+    async fn build_and_send(&mut self, command: &str, arguments: Option<Value>, include_stream_id: bool) -> Result<(), XtbStreamConnectionError> {
+        let arguments_obj = Self::prepare_arguments(arguments)?;
+        let message = self.build_stream_message(command.to_owned(), include_stream_id, arguments_obj)?;
+        self.sink.send(message).await.map_err(|err| XtbStreamConnectionError::CannotSend(err))
+    }
+
+    /// Build message for subscription
+    fn build_stream_message(&self, command: String, include_stream_id: bool, payload: Option<Map<String, Value>>) -> Result<Message, XtbStreamConnectionError> {
+        let mut content = Map::new();
+        content.insert("command".to_string(), Value::String(command.to_string()));
+        if include_stream_id {
+            content.insert("streamSessionId".to_string(), Value::String(self.stream_session_id.clone()));
+        }
+        if let Some(mut payload_data) = payload {
+            content.append(&mut payload_data);
+        }
+        Ok(Message::Text(serde_json::to_string(&Value::Object(content)).map_err(|err| XtbStreamConnectionError::SerializationFailed(err))?))
+    }
+
+    fn prepare_arguments(arguments: Option<Value>) -> Result<Option<Map<String, Value>>, XtbStreamConnectionError> {
+        match arguments {
+            None => Ok(None),
+            Some(Value::Object(obj)) => Ok(Some(obj)),
+            _ => Err(XtbStreamConnectionError::InvalidArgumentsType)
+        }
+    }
 }
 
 
 #[async_trait]
 impl XtbStreamConnection for BasicXtbStreamConnection {
-    async fn subscribe(&mut self, command: &str, arguments: Value) -> Result<(), XtbStreamConnectionError> {
-        todo!()
+    async fn subscribe(&mut self, command: &str, arguments: Option<Value>) -> Result<(), XtbStreamConnectionError> {
+        self.build_and_send(command, arguments, true).await
     }
 
-    async fn unsubscribe(&mut self, command: &str, arguments: Value) -> Result<(), XtbStreamConnectionError> {
-        todo!()
+    async fn unsubscribe(&mut self, command: &str, arguments: Option<Value>) -> Result<(), XtbStreamConnectionError> {
+        self.build_and_send(command, arguments, false).await
     }
 
     async fn make_message_stream(&mut self, filter: StreamFilter) -> MessageStream {
@@ -67,7 +96,9 @@ impl XtbStreamConnection for BasicXtbStreamConnection {
 }
 
 
+#[derive(Default)]
 pub enum StreamFilter {
+    #[default]
     Noop,
     Command(String),
     And(Vec<StreamFilter>),
@@ -81,7 +112,13 @@ pub enum StreamFilter {
 pub enum XtbStreamConnectionError {
     #[error("Cannot connect to server ({0}")]
     CannotConnect(String),
+    #[error("Cannot send message")]
+    CannotSend(tokio_tungstenite::tungstenite::Error),
+    #[error("Cannot serialize data")]
+    SerializationFailed(serde_json::Error),
+    #[error("Only Value::Object can be used for the arguments")]
+    InvalidArgumentsType,
 }
 
 
-struct MessageStream {}
+pub struct MessageStream {}
