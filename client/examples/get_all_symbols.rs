10502ef8099b137d14ee007e296d69da49c09f90
Petr Jindra <petr@gbol.cz>
2024-03-17 20:40:17 +0100

    the examples directory and example downloading all symbols and printing it into console

---
+++ client/examples/get_all_symbols.rs
@@ -0,0 +1,20 @@
+use tracing::Subscriber;
+use xtb_client::ApiClient;
+use xtb_client::schema::GetAllSymbolsRequest;
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().unwrap_or_default();
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    let username = dotenvy::var("XTB_USERNAME").unwrap();
+    let password = dotenvy::var("XTB_PASSWORD").unwrap();
+    let api_server = dotenvy::var("XTB_API_SERVER").unwrap();
+    let stream_server = dotenvy::var("XTB_STREAM_SERVER").unwrap();
+
+    let mut client = xtb_client::XtbClientBuilder::new(&api_server, &stream_server).build(&username, &password).await.unwrap();
+
+    let symbols = client.get_all_symbols(GetAllSymbolsRequest::default()).await.unwrap();
+    println!("{}", serde_json::to_string_pretty(&symbols).unwrap())
+}
